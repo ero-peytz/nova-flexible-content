b1a974b17bee75b9ec4132a3fa42756376444584
Mads Hansen <mads.hansen@dk-c02th0a5gtdx.mgntx.local>
2019-09-27 15:45:09 +0200

    Add flex-wrap

--- resources/js/components/FormGroup.vue
+++ resources/js/components/FormGroup.vue
@@ -79,7 +79,7 @@ export default {
             return classes;
         },
         containerStyle() {
-            let classes = ['flex-grow', 'border-b', 'border-r', 'border-l', 'border-60', 'rounded-b-lg', 'flex'];
+            let classes = ['flex-grow', 'border-b', 'border-r', 'border-l', 'border-60', 'rounded-b-lg', 'flex', 'flex-wrap'];
             if(!this.group.title) {
                 classes.push('border-t');
                 classes.push('rounded-tr-lg');
