ee5284e931805bb14fafa37fec9847a32811a3a6
Emil Ravnsholt <ero@peytz.dk>
2024-06-24 14:30:20 +0200

    Upgrading to v1.1.1 (latest from original package)

--- resources/js/components/OriginalDropMenu.vue
+++ resources/js/components/OriginalDropMenu.vue
@@ -2,7 +2,8 @@
     <div class="relative" v-if="layouts">
         <div v-if="isLayoutsDropdownOpen && layouts.length > 1"
                 ref="dropdown"
-                class="z-20 absolute rounded-lg shadow-lg max-w-full max-h-search overflow-y-auto border border-40 mb-3 pin-t"
+                class="z-20 absolute rounded-lg shadow-lg max-w-full max-h-search overflow-y-auto border border-40"
+                v-bind:class="dropdownClasses"
         >
             <ul class="list-reset">
                 <li v-for="layout in filteredLayouts" class="border-b border-gray-100 dark:border-gray-700" :key="'add-'+layout.name">
@@ -54,6 +55,15 @@
             isBelowLayoutLimits() {
                 return (this.limitCounter > 0 || this.limitCounter === null) && this.filteredLayouts.length > 0;
             },
+
+            dropdownClasses() {
+                return {
+                    'mt-3': this.dropdownOrientation === 'bottom',
+                    'pin-b': this.dropdownOrientation === 'bottom',
+                    'mb-3': this.dropdownOrientation === 'top',
+                    'pin-t': this.dropdownOrientation === 'top',
+                };
+            }
         },
 
         methods: {
